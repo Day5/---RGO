e67ce420c51133c28d388b17c6fa9f16f9abc4bd
梵团子 <937351823@qq.com>
2016-11-23 11:31:43 +0800

    Create index.js

---
+++ pages/index/index.js
@@ -0,0 +1,23 @@
+Page({
+    data:{
+        imgUrl:[
+            "../../images/banner1.jpg",
+            "../../images/banner2.jpg",
+            "../../images/banner3.jpg"
+        ],
+        indicatorDots:true,
+        autoplay:true,
+        interval:5000,
+        duration:1000,
+        menu:[
+           {"url":"../../images/all.png","text":"所有商品"},
+           {"url":"../../images/shop.png", "text":"店铺街"},
+           {"url":"../../images/brand.png", "text":"品牌街"},
+           {"url":"../../images/discount.png", "text":"优惠活动"},
+           {"url":"../../images/sale.png", "text":"促销活动"},
+           {"url":"../../images/point.png", "text":"积分商城"},
+           {"url":"../../images/basket-m.png", "text":"购物车"},
+           {"url":"../../images/myaccount.png","text":"个人中心"}
+        ]
+    }
+})
